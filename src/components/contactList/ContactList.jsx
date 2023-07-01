b6d546175a662f9b73544707e6cb878c3556ea3a
OleksandrDomin <92186960+OleksandrDomin@users.noreply.github.com>
2023-07-01 18:13:46 +0200

    after last

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -4,17 +4,15 @@ import css from "./ContactList.module.css";
 
 
 function ContactList({ contacts, onDeleteContact }) {
-   
-   
     return (
         <div >
-            {contacts.length> 0 ? ( <ul className={css.contactList_list}>
-                {contacts.map(contact =>  
-                    <li className={css.contactList_item}  key={contact.id}> <span >{contact.name}:{contact.number}</span>
-                    <button className={css.button} onClick={()=> onDeleteContact(contact.id)}>Delete</button>
-                </li>                
+     <ul className={css.contactList_list}>
+        {contacts.map(contact =>  
+        <li className={css.contactList_item}  key={contact.id}> <span >{contact.name}:{contact.number}</span>
+        <button className={css.button} onClick={()=> onDeleteContact(contact.id)}>Delete</button>
+        </li>                
          )}
-        </ul>): (<h3>You have No contacts, please enter contacts.....</h3>)}
+        </ul>
         </div>
     )
 };
